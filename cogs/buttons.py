2394af70dd1fe105b94d1cf61621a6efbde80d66
kalebbroo <kalebbroo@gmail.com>
2023-05-26 00:56:54 -0400

    refactor butons

--- cogs/buttons.py
+++ cogs/buttons.py
@@ -1,3 +1,4 @@
+from cogs.text2image import text2image
 from discord.ext import commands
 from discord import ButtonStyle, Interaction
 from discord.ui import Button, View
@@ -7,42 +8,48 @@ class Buttons(commands.Cog):
         self.bot = bot
 
     class ImageView(View):
-        def __init__(self, ctx, image_urls):
+        def __init__(self, ctx, image_urls, settings):
             super().__init__()
             self.ctx = ctx
             self.image_urls = image_urls
+            self.settings = settings
 
-        @Button(label="Choose for img2img", style=ButtonStyle.primary)
-        async def choose_for_img2img(self, button: Button, interaction: Interaction):
+            # Add buttons to the view with custom ids
+            self.add_item(Button(style=ButtonStyle.secondary, label="Choose img", custom_id="choose_img"))
+            self.add_item(Button(style=ButtonStyle.primary, label="Upscale", custom_id="upscale"))
+            self.add_item(Button(style=ButtonStyle.primary, label="Regenerate", custom_id="regenerate"))
+            self.add_item(Button(style=ButtonStyle.danger, label="Delete", custom_id="delete"))
+            self.add_item(Button(style=ButtonStyle.secondary, label="Edit Prompt", custom_id="edit_prompt"))
+            self.add_item(Button(style=ButtonStyle.secondary, label="Randomize Seed", custom_id="randomize_seed"))
+            self.add_item(Button(style=ButtonStyle.secondary, label="View Info", custom_id="view_info"))
+
+        async def choose_img(self, button: Button, interaction: Interaction):
             # Here you would implement the logic for choosing one of the images for img2img
             pass
 
-        @Button(label="Upscale and save", style=ButtonStyle.primary)
-        async def upscale_and_save(self, button: Button, interaction: Interaction):
+        async def upscale(self, button: Button, interaction: Interaction):
             # Here you would implement the logic for upscaling and saving the image
             pass
 
-        @Button(label="Regenerate", style=ButtonStyle.primary)
         async def regenerate(self, button: Button, interaction: Interaction):
-            # Here you would implement the logic for regenerating the image
-            pass
+            # Regenerate the image using the stored settings
+            new_image = text2image(**self.settings)
+            # Update the message with the new image
+            await self.ctx.message.edit(content=new_image)
+            
 
-        @Button(label="Delete", style=ButtonStyle.danger)
         async def delete(self, button: Button, interaction: Interaction):
             # Delete the message
             await self.ctx.message.delete()
 
-        @Button(label="Edit Prompt", style=ButtonStyle.secondary)
         async def edit_prompt(self, button: Button, interaction: Interaction):
             # Here you would implement the logic for editing the prompt
             pass
 
-        @Button(label="Randomize Seed", style=ButtonStyle.secondary)
         async def randomize_seed(self, button: Button, interaction: Interaction):
             # Here you would implement the logic for randomizing the seed
             pass
 
-        @Button(label="View Info", style=ButtonStyle.secondary)
         async def view_info(self, button: Button, interaction: Interaction):
             # Here you would implement the logic for viewing the image's information
             pass
@@ -51,6 +58,24 @@ class Buttons(commands.Cog):
         # Create the view and add it to the message
         view = self.ImageView(ctx, image_urls)
         await ctx.message.edit(view=view)
+
+    @commands.Cog.listiner()
+    async def on_interaction(self, interaction: Interaction):
+        match interaction.data["custom_id"]:
+            case "choose_img":
+                await interaction.view.choose_img(interaction)
+            case "upscale":
+                await interaction.view.upscale(interaction)
+            case "regenerate":
+                await interaction.view.regenerate(interaction)
+            case "delete":
+                await interaction.view.delete(interaction)
+            case "edit_prompt":
+                await interaction.view.edit_prompt(interaction)
+            case "randomize_seed":
+                await interaction.view.randomize_seed(interaction)
+            case "view_info":
+                await interaction.view.view_info(interaction)
 
 def setup(bot: commands.Bot) -> None:
     bot.add_cog(Buttons(bot))
