fafef8b492736595de3f67b315f5c18e18aebdb6
kalebbroo <kalebbroo@gmail.com>
2023-05-25 23:16:20 -0400

    added GPT rewrite function

--- cogs/GPT_prompt.py
+++ cogs/GPT_prompt.py
@@ -1,14 +1,28 @@
-# add code so the user can use Chat GPT to give them a better prompt
 from discord.ext import commands
-from discord import app_commands
+from openai import OpenAI, GPT3Completion
 
-
-
-class GPT(commands.Cog):
-    bot: commands.Bot
-
+class PromptRewriter(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.openai = OpenAI("YOUR_OPENAI_API_KEY")  # replace with your OpenAI API key
 
-async def setup(bot: commands.Bot) -> None:
-    await bot.add_cog(GPT(bot))
+    @commands.command(
+        name="rewrite_prompt",
+        description="Rewrite a prompt for generating an image with Stable Diffusion",
+    )
+    async def rewrite_prompt(self, ctx, *, original_prompt: str):
+        pre_prompt = "You are an expert prompt creator for making prompts for Stable Diffusion to create amazing images. Rewrite this prompt to make the best quality image possible for what I'm looking for. Make sure to create a detailed and long prompt so it will correctly generate the image I want."
+        full_prompt = f"{pre_prompt}\n\nOriginal Prompt: {original_prompt}\n\nRewritten Prompt:"
+
+        response = self.openai.complete(
+            model="text-davinci-002",
+            prompt=full_prompt,
+            max_tokens=100,
+            temperature=0.5,
+        )
+
+        rewritten_prompt = response.choices[0].text.strip()
+        await ctx.send(f"Rewritten Prompt: {rewritten_prompt}")
+
+def setup(bot: commands.Bot) -> None:
+    bot.add_cog(PromptRewriter(bot))
