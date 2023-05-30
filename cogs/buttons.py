86827ab26b2fae0561e3c86299d53393cacada06
kalebbroo <kalebbroo@gmail.com>
2023-05-30 00:35:15 -0400

    added upscale image. Working on adding img2img.

--- cogs/buttons.py
+++ cogs/buttons.py
@@ -1,58 +1,91 @@
-from cogs.text2image import text2image
+import os
+import discord
 from discord.ext import commands
 from discord import ButtonStyle, Interaction
-from discord.ui import Button, View
+from discord.ui import Button, View, Select
 import asyncio
+
+
+
 
 class Buttons(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+
     class ImageView(View):
-        def __init__(self, ctx, image_urls, settings):
+        def __init__(self, ctx, image_urls, payload):
             super().__init__()
             self.ctx = ctx
             self.image_urls = image_urls
-            self.settings = settings
+            self.payload = payload
 
             # Add buttons to the view with custom ids
             self.add_item(Button(style=ButtonStyle.secondary, label="Choose img", custom_id="choose_img"))
             self.add_item(Button(style=ButtonStyle.primary, label="Upscale", custom_id="upscale"))
-            self.add_item(Button(style=ButtonStyle.primary, label="Regenerate", custom_id="regenerate"))
+            self.add_item(Button(style=ButtonStyle.success, label="Regenerate", custom_id="regenerate"))
             self.add_item(Button(style=ButtonStyle.danger, label="Delete", custom_id="delete"))
             self.add_item(Button(style=ButtonStyle.secondary, label="Edit Prompt", custom_id="edit_prompt"))
-            self.add_item(Button(style=ButtonStyle.secondary, label="Randomize Seed", custom_id="randomize_seed"))
-            self.add_item(Button(style=ButtonStyle.secondary, label="View Info", custom_id="view_info"))
 
-        async def choose_img(self, interaction: Interaction, button: Button):
-            # Here you would implement the logic for choosing one of the images for img2img
-            pass
 
-        async def upscale(self, interaction: Interaction, button: Button):
-            # Here you would implement the logic for upscaling and saving the image
-            pass
+    class SelectMenuView(discord.ui.View):
+        def __init__(self, select_menu):
+            super().__init__()
+            self.add_item(select_menu)
 
-        async def regenerate(self, interaction: Interaction, button: Button):
-            # Regenerate the image using the stored settings
-            new_image = text2image(**self.settings)
-            # Update the message with the new image
-            await self.ctx.message.edit(content=new_image)
 
-        async def delete(self, interaction: Interaction, button: Button):
-            # Delete the message
-            await self.ctx.message.delete()
+    class ImageSelect(Select):
+        def __init__(self, bot, image_files):
+            self.bot = bot
+            options = []
+            for image_file in image_files:
+                # Create a SelectOption for each image file
+                option = discord.SelectOption(label=os.path.basename(image_file), value=image_file)
+                options.append(option)
 
-        async def edit_prompt(self, interaction: Interaction, button: Button):
-            # Here you would implement the logic for editing the prompt
-            pass
+            super().__init__(placeholder='Images are labeled 0-3 from left to right', options=options)
 
-        async def randomize_seed(self, interaction: Interaction, button: Button):
-            # Here you would implement the logic for randomizing the seed
-            pass
 
-        async def view_info(self, interaction: Interaction, button: Button):
-            # Here you would implement the logic for viewing the image's information
-            pass
+        async def callback(self, interaction: Interaction):
+            # Get the selected image file
+            selected_image_file = self.values[0]
+
+            # Generate more images from the selected image
+            generated_images = await self.bot.get_cog('Image2Image').generate_images(selected_image_file, interaction)
+
+            # Create a new embed with a grid of images
+            embed = discord.Embed(title="Generated Images")
+            for i, image_path in enumerate(generated_images):
+                with open(image_path, 'rb') as f:
+                    file = discord.File(f, filename=f"image{i}.png")
+                    embed.set_image(url=f"attachment://image{i}.png")
+                    await interaction.channel.send(embed=embed, file=file)
+
+    class UpscaleSelect(Select):
+        def __init__(self, bot, image_files):
+            self.bot = bot
+            options = []
+            for image_file in image_files:
+                # Create a SelectOption for each image file
+                option = discord.SelectOption(label=os.path.basename(image_file), value=image_file)
+                options.append(option)
+
+            super().__init__(placeholder='Images are labeled 0-3 from left to right', options=options)
+
+
+        async def callback(self, interaction: Interaction):
+            # Get the selected image file
+            selected_image_file = self.values[0]
+
+            # Call the upscale_image function to upscale the image
+            upscaled_image_path = await self.bot.get_cog('Image2Image').upscale_image(selected_image_file, interaction)
+
+            # Display the upscaled image
+            with open(upscaled_image_path, 'rb') as f:
+                upscaled_image = discord.File(f)
+                await interaction.channel.send(file=upscaled_image)
+
+
 
     async def create_view(self, ctx, image_urls, settings):
         # Create the view and add it to the message
@@ -61,21 +94,56 @@ class Buttons(commands.Cog):
 
     @commands.Cog.listener()
     async def on_interaction(self, interaction: Interaction):
-        match interaction.data["custom_id"]:
-            case "choose_img":
-                await interaction.view.choose_img(interaction)
-            case "upscale":
-                await interaction.view.upscale(interaction)
-            case "regenerate":
-                await interaction.view.regenerate(interaction)
-            case "delete":
-                await interaction.view.delete(interaction)
-            case "edit_prompt":
-                await interaction.view.edit_prompt(interaction)
-            case "randomize_seed":
-                await interaction.view.randomize_seed(interaction)
-            case "view_info":
-                await interaction.view.view_info(interaction)
+        if interaction.type == discord.InteractionType.component:
+            button_id = interaction.data["custom_id"]
+            match button_id:
+                case "choose_img":
+                    await interaction.response.defer()
+                    image_files = [os.path.join("image_cache", image_file) for image_file in os.listdir("image_cache")]
+                    select_menu = self.ImageSelect(self.bot, image_files)
+                    select_menu_view = self.SelectMenuView(select_menu)
+                    await interaction.channel.send("Select an image to generate more from.", view=select_menu_view)
 
-async def setup(bot: commands.Bot) -> None:
+                case "upscale":
+                    await interaction.response.defer()
+                    image_files = [os.path.join("image_cache", image_file) for image_file in os.listdir("image_cache")]
+                    select_menu = self.UpscaleSelect(self.bot, image_files)
+                    select_menu_view = self.SelectMenuView(select_menu)
+                    await interaction.channel.send("Select an image to upscale so you can save it.", view=select_menu_view)
+
+
+                case "regenerate":
+                    #await self.bot.get_cog('Buttons').regenerate(interaction)
+                    await interaction.response.defer()
+                    await interaction.followup.send("Regenerating...")
+                    # Regenerate the image using the stored payload
+                    image_data = await self.bot.get_cog('Text2Image').txt2image(self.payload)
+                    image_file = await self.bot.get_cog('Text2Image').pull_image(image_data)
+
+                    buttons = self.bot.get_cog('Buttons').ImageView(interaction, image_data['images'], self.payload)
+
+                    # Create the embed
+                    embed = await self.bot.get_cog('Commands').create_embed(interaction, self.payload['prompt'], self.payload['negative_prompt'], 
+                                                                            self.payload['steps'], self.payload['seed'], self.payload['cfg_scale'])
+                    # Update the message with the new image
+                    await interaction.channel.send(embed=embed, file=image_file, view=buttons)
+
+
+                case "delete":
+                    await interaction.response.defer()
+                    # Delete the message
+                    await interaction.message.delete()
+                    await interaction.response.channel.send("Embed deleted", ephemeral=True)
+
+
+                case "edit_prompt":
+                    await self.edit_prompt(interaction)
+
+
+                    
+
+
+
+
+async def setup(bot):
     await bot.add_cog(Buttons(bot))
--- cogs/img2img.py
+++ cogs/img2img.py
@@ -1,9 +1,193 @@
+import aiohttp
+import base64
+import os
+from io import BytesIO
+from PIL import Image
 from discord.ext import commands
+import requests
 
-class Img2Img(commands.Cog):
+class Image2Image(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-async def setup(self) -> None:
-    await self.bot.add_cog(Img2Img(self.bot))
+    @staticmethod
+    def create_payload(init_images, resize_mode=0, denoising_strength=0.75, image_cfg_scale=0, mask="", mask_blur=4,
+                       inpainting_fill=0, inpaint_full_res=True, inpaint_full_res_padding=0, inpainting_mask_invert=0,
+                       initial_noise_multiplier=0, prompt="", styles=[], seed=-1, subseed=-1, subseed_strength=0,
+                       seed_resize_from_h=-1, seed_resize_from_w=-1, sampler_name="DPM++ 2S a Karras", batch_size=1, n_iter=1,
+                       steps=50, cfg_scale=7, width=512, height=512, restore_faces=False, tiling=False,
+                       do_not_save_samples=False, do_not_save_grid=False, negative_prompt="", eta=0,
+                       s_min_uncond=0, s_churn=0, s_tmax=0, s_tmin=0, s_noise=1, override_settings={},
+                       override_settings_restore_afterwards=True, script_args=[], sampler_index="Euler",
+                       include_init_images=False, script_name="", send_images=True, save_images=False,
+                       alwayson_scripts={}):
+        return {
+            "init_images": init_images,
+            "resize_mode": resize_mode,
+            "denoising_strength": denoising_strength,
+            "image_cfg_scale": image_cfg_scale,
+            "mask": mask,
+            "mask_blur": mask_blur,
+            "inpainting_fill": inpainting_fill,
+            "inpaint_full_res": inpaint_full_res,
+            "inpaint_full_res_padding": inpaint_full_res_padding,
+            "inpainting_mask_invert": inpainting_mask_invert,
+            "initial_noise_multiplier": initial_noise_multiplier,
+            "prompt": prompt,
+            "styles": styles,
+            "seed": seed,
+            "subseed": subseed,
+            "subseed_strength": subseed_strength,
+            "seed_resize_from_h": seed_resize_from_h,
+            "seed_resize_from_w": seed_resize_from_w,
+            "sampler_name": sampler_name,
+            "batch_size": batch_size,
+            "n_iter": n_iter,
+            "steps": steps,
+            "cfg_scale": cfg_scale,
+            "width": width,
+            "height": height,
+            "restore_faces": restore_faces,
+            "tiling": tiling,
+            "do_not_save_samples": do_not_save_samples,
+            "do_not_save_grid": do_not_save_grid,
+            "negative_prompt": negative_prompt,
+            "eta": eta,
+            "s_min_uncond": s_min_uncond,
+            "s_churn": s_churn,
+            "s_tmax": s_tmax,
+            "s_tmin": s_tmin,
+            "s_noise": s_noise,
+            "override_settings": override_settings,
+            "override_settings_restore_afterwards": override_settings_restore_afterwards,
+            "script_args": script_args,
+            "sampler_index": sampler_index,
+            "include_init_images": include_init_images,
+            "script_name": script_name,
+            "send_images": send_images,
+            "save_images": save_images,
+            "alwayson_scripts": alwayson_scripts
+        }
+
+
+    @staticmethod
+    async def img2img(payload):
+        url = "http://localhost:7860/sdapi/v1/img2img"
+        headers = {"Content-Type": "application/json"}
+
+        print(f"Payload: {payload}")
+
+        async with aiohttp.ClientSession() as session:
+            async with session.post(url, headers=headers, json=payload) as response:
+                if response.status == 200:
+                    response_data = await response.json()
+                    return response_data
+                else:
+                    print(f"Error: {response.status}")
+                    return None
+
+    async def upscale_image(self, image_path, interaction):
+        try:
+            # Open the image and convert it to base64
+            with open(image_path, "rb") as image_file:
+                encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+                # Add the prefix
+                encoded_string = "data:image/png;base64," + encoded_string
+
+        except FileNotFoundError:
+            await interaction.response.send_message("The image file could not be found.", ephemeral=True)
+            return None
+        except OSError:
+            await interaction.response.send_message("The file is not a valid image file.", ephemeral=True)
+            return None
+
+        # Create the payload
+        #payload = self.create_payload(init_images=[encoded_string], resize_mode=2)
+
+        # Build the request payload
+        """{
+            "resize_mode": 0,
+            "show_extras_results": true,
+            "gfpgan_visibility": 0,
+            "codeformer_visibility": 0,
+            "codeformer_weight": 0,
+            "upscaling_resize": 2,
+            "upscaling_resize_w": 512,
+            "upscaling_resize_h": 512,
+            "upscaling_crop": true,
+            "upscaler_1": "None",
+            "upscaler_2": "None",
+            "extras_upscaler_2_visibility": 0,
+            "upscale_first": false,
+            "image": ""
+            }"""
+        payload = {
+            "upscaling_resize": 4,
+            "upscaler_1": "R-ESRGAN 4x+",
+            "image": encoded_string,
+            "upscale_first": True
+        }
+
+        # Send the request to the img2img API
+        #response = await self.img2img(payload)
+        # Create a session and send the request to the API
+        async with aiohttp.ClientSession() as session:
+            async with session.post("http://localhost:7860/sdapi/v1/extra-single-image", json=payload) as response:
+                response_json = await response.json()
+            with open('response.txt', 'w') as file:
+                file.write(str(response_json))
+            
+
+
+        # If the response is not None, save the upscaled image
+        if response is not None:
+            # Get the base64 string of the upscaled image from the response
+            upscaled_image_data = response_json['image']
+
+
+            # Convert the base64 string back to an image
+            upscaled_image = Image.open(BytesIO(base64.b64decode(upscaled_image_data)))
+
+            # Save the upscaled image and return the path
+            folder_path = "image_cache/upscaled_images"
+            os.makedirs(folder_path, exist_ok=True)
+            upscaled_image_path = os.path.join(folder_path, f"upscaled_image.png")
+            upscaled_image.save(upscaled_image_path)
+
+            return upscaled_image_path
+        else:
+            await interaction.response.send_message("Failed to upscale the image.", ephemeral=True)
+            return None
+        
+    async def create_img2img(self, image_path, interaction):
+        # Read the image file and encode it to base64
+        with open(image_path, "rb") as image_file:
+            encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+
+        # Create the payload for the API request
+        payload = self.create_payload(encoded_string)
+
+        # Make the API request
+        response = requests.post('http://localhost:7860/sdapi/v1/img2img', json=payload)
+
+        if response.status_code == 200:
+            # Decode the response and save the upscaled image
+            response_data = response.json()
+            upscaled_images_data = response_data['images']
+
+            # Decode each image and convert it to a PIL Image object
+            upscaled_images = [Image.open(BytesIO(base64.b64decode(image_data))) for image_data in upscaled_images_data]
+
+            # Save each upscaled image in the 'image_cache' folder
+            for i, image in enumerate(upscaled_images):
+                upscaled_image_path = os.path.join("image_cache", f"upscaled_image_{i}.png")
+                image.save(upscaled_image_path)
+
+            return upscaled_images
+        else:
+            await interaction.response.send_message("Failed to generate upscaled images.", ephemeral=True)
+            return None
+
+async def setup(bot):
+    await bot.add_cog(Image2Image(bot))
 
