8fd7668ff331a92432798fc16b55709c7e689915
kalebbroo <kalebbroo@gmail.com>
2023-11-29 01:16:09 -0500

    typo

--- cogs/commands.py
+++ cogs/commands.py
@@ -126,7 +126,7 @@ class Commands(commands.Cog):
             await api_call.call_collect(interaction, payload)
             
     async def image_to_base64(image_path):
-        with open(image_path, "rb") as image_file:
+        with open(image_path) as image_file:
             encoded_string = base64.b64encode(image_file.read()).decode("utf-8")
         return encoded_string
     
